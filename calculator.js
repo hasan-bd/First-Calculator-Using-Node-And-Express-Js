3039a8295a045bc59f1a3246b7e8e843e79d3d33
kamrulhasan57 <kamrulhasan570@gmail.com>
2021-03-10 00:33:50 +0600

    Adding bmiCalculator get and post code and adding routes

--- calculator.js
+++ calculator.js
@@ -10,12 +10,26 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + "/index.html")
 })
 
+app.get('/hasan', (req, res) => {
+  res.sendFile(__dirname + "/bmiCalculator.html")
+})
+
+
 app.post('/',function(req, res){
   var number1 = Number(req.body.num1)
   var number2 = Number(req.body.num2)
   var result = number1 + number2
   res.send("The result is " + result)
 })
+
+app.post('/bmicalculator',function(req, res){
+  var number1 = parseFloat(req.body.num1)
+  var number2 = Math.pow(parseFloat(req.body.num2), 2)
+  var result = number1/number2
+  res.send("The BMI result is " + result)
+
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
